79c321f5263e79773f6b0e6ebd22178a8b29068a
promise <emmanuelpromise456@gmail.com>
2024-03-08 12:54:06 +0100

    update 2 files and delete 1 file

---
+++ public/script.js
@@ -0,0 +1,22 @@
+const greetingsElement = document.querySelector('.greetings');
+const greetingsText = [
+  "By signing up, you're taking the first step towards unlocking a world of productivity and organization.",
+  "Let's embark on this journey together and make every task count.",
+  "Start by creating your account and let's begin organizing your life!"
+];
+let currentSentence = 0;
+let typed = 0;
+
+const typeWriter = () => {
+  if (typed < greetingsText[currentSentence].length) {
+    greetingsElement.textContent = greetingsText[currentSentence].substring(0, typed + 1);
+    typed++;
+    setTimeout(typeWriter, 20); // Adjust speed here (ms between keystrokes)
+  } else {
+    typed = 0;
+    currentSentence = (currentSentence + 1) % greetingsText.length; // Loop through sentences
+    setTimeout(typeWriter, 1000); // Pause between sentences (ms)
+  }
+};
+
+typeWriter();
