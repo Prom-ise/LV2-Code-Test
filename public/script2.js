67e18a83f534c0fc744ff7f9b234cc9579eaad44
promise <emmanuelpromise456@gmail.com>
2024-03-08 15:19:51 +0100

    update dashboard.html and script2.js

--- public/script2.js
+++ public/script2.js
@@ -1,6 +1,8 @@
 document.getElementById('show')
+document.getElementById('showy')
 document.getElementById('todoBox')
  const openWork = () => {
   show.style.display = 'none'
+  showy.style.display = 'block'
   todoBox.style.display = 'block'
  }
